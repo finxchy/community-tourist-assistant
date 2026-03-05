7e2c9ac5f5d0fd654a74444f5b847a60dd63de68
Finn Bullingham <finn.bullingham@access.coombe.local>
2026-03-05 13:20:00 +0000

    Add place code and html added, additionally, tourist detail places on chosen attractions / loactions begun

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, add_place, place_detail
 
 urlpatterns = [
     path("", home, name="home"),
+    path("add/", add_place, name="add_place"),
+    path("place/<int:place_id>/", place_detail, name="place_detail"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Place
+from .form import PlaceForm
 
 def home(request):
     places = Place.objects.all()
     return render(request, "home.html", {"places": places})
+
+def add_place(request):
+    if request.method == "POST":
+        form = PlaceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = PlaceForm()
+    return render(request, "add_place.html", {"form": form})
+
+def place_detail(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    return render(request, "place_detail.html", {"place": place})
