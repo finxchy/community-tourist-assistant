9e1c37df20c6d8493dabc702e5412293b6e28955
Finn Bullingham <finn.bullingham@access.coombe.local>
2026-03-05 15:40:25 +0000

    Location functionlaity and filename readmes created for the html to get the png images

--- core/models.py
+++ core/models.py
@@ -4,5 +4,7 @@ class Place(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
 
+    image_filename = models.CharField(max_length=200, blank=True, default="")
+
     def __str__(self):
         return self.name
