820532678e7a6a7742f2192f9943722b2ddb9252
Finn Bullingham <finn.bullingham@access.coombe.local>
2026-03-06 10:47:14 +0000

    Location functionlaity and filename readmes created for the html to get the png images

--- core/models.py
+++ core/models.py
@@ -3,8 +3,18 @@ from django.db import models
 class Place(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+    town = models.CharField(max_length=200, blank=True, default="")
 
     image_filename = models.CharField(max_length=200, blank=True, default="")
 
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="reviews")
+    reviewer_name = models.CharField(max_length=100)
+    rating = models.IntegerField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"{self.reviewer_name} - {self.place.name}"
