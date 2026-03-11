3d48762952e8e0ca79809e7649387f75140837b0
Finn Bullingham <finn.bullingham@access.coombe.local>
2026-03-11 14:44:04 +0000

    Added a search functionality

--- core/views.py
+++ core/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from .models import Place
 from .form import PlaceForm, ReviewForm
 
 def home(request):
+    query = request.GET.get("q", "")
     places = Place.objects.all()
-    return render(request, "home.html", {"places": places})
+
+    if query:
+        places = places.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query) |
+            Q(town__icontains=query)
+        )
+
+    return render(request, "home.html", {
+        "places": places,
+        "query": query,
+    })
 
 def add_place(request):
     if request.method == "POST":
